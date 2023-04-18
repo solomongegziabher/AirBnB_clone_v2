4a0e133b972be31405e57d4b6e50d9e6556bb757
solomongegziabher <sage.h22@gmail.com>
2023-04-18 05:01:31 -0700

    AirBnB clone v2

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -5,15 +5,18 @@ from models.amenity import Amenity
 
 
 class test_Amenity(test_basemodel):
-    """ """
+    """amenity test class"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """Initializes the test class"""
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
     def test_name2(self):
-        """ """
+        """Tests the type of name """
         new = self.value()
-        self.assertEqual(type(new.name), str)
+        self.assertEqual(
+                type(new.name), 
+                str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
+                )
