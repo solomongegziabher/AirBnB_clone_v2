041c882d2cf70c69f685fb87272a247c37ca960e
solomongegziabher <sage.h22@gmail.com>
2023-05-22 12:32:44 -0700

    web flask

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,31 +1,26 @@
 #!/usr/bin/python3
-"""starts a Flask web application
-"""
+"""Importing Flask to run the web app"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def close_db(exc):
-    """closes
-    """
+def close(self):
+    """ Method to close the session """
     storage.close()
 
 
-@app.route('/cities_by_states')
+@app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """display a HTML page
-    """
-    states = storage.all(State).values()
-    return render_template("8-cities_by_states.html", states=states)
+    """Displays a html page with states and cities"""
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port="5000")
 
